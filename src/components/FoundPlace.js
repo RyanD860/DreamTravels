5fb9ea2d8b4033bc06f826bf736575a06ed89d4b
RyanD860 <31831221+RyanD860@users.noreply.github.com>
2018-07-01 19:33:13 -0400

    Chaning not found on search

--- src/components/FoundPlace.js
+++ src/components/FoundPlace.js
@@ -7,13 +7,42 @@ class FoundPlace extends Component {
     super(props);
 
     this.state = {
-      image: ""
+      image: "",
+      name: ""
     };
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.place.photos !== prevProps.place.photos) {
-      this.setState({ image: this.props.place.photos[0].photo_reference });
+      if (this.props.place.photos) {
+        this.setState({
+          image: this.props.place.photos[0].photo_reference
+        });
+      } else {
+        this.setState({
+          image: "No Image"
+        });
+      }
+    }
+    if (this.props.place.name !== prevProps.place.name) {
+      this.setState({
+        name: this.props.place.name
+      });
+    }
+  }
+  displayImg() {
+    if (this.state.image !== "No Image") {
+      return (
+        <img
+          src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=${
+            this.state.image
+          }&key=${config.placesApiKey}`}
+        />
+      );
+    } else {
+      return (
+        <img src="https://i5.walmartimages.com/asr/f752abb3-1b49-4f99-b68a-7c4d77b45b40_1.39d6c524f6033c7c58bd073db1b99786.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF" />
+      );
     }
   }
 
@@ -21,15 +50,11 @@ class FoundPlace extends Component {
     return (
       <div>
         <h3>{this.props.place.name}</h3>
-        {this.state.image === "" ? (
+        {this.state.name === "" ? (
           false
         ) : (
           <div>
-            <img
-              src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=600&photoreference=${
-                this.state.image
-              }&key=${config.placesApiKey}`}
-            />
+            {this.displayImg()}
             <button
               onClick={() =>
                 this.props.addToWant(
